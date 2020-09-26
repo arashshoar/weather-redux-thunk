f3b70605cb1532c9aab0dc7a4813cd457f8e9d71
Arash Soudishoar <b_arash_s@yahoo.com>
2020-09-26 07:34:14 -0700

    test if heroku is fine with whole application and resources like images

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import styles from './App.module.scss';
 const App = () => (
     <div className={styles.app}>
       <div>In The Name of GOD</div>
+      <img src='https://s.yimg.com/os/weather/1.0.1/precipitation/54x60/rain_ico_100@2x.png' />
 
     </div>
 );
